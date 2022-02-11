42bd0f49eb9d0181cdc18bbd7e78473b38f6ba83
Adam Smith <adams@cipafilter.com>
2022-02-11 15:10:58 -0600

    WIP

--- main/go.mod
+++ main/go.mod
@@ -2,4 +2,4 @@ module github.com/theotheradamsmith/verbose-octo-eureka/main
 
 go 1.17
 
-require github.com/theotheradamsmith/verbose-octo-eureka/logic v0.1.1
+require github.com/theotheradamsmith/verbose-octo-eureka/logic v1.0.0
--- main/main.go
+++ main/main.go
@@ -9,13 +9,13 @@ import (
 	"os"
 	"strconv"
 	"strings"
-)
 
-//"github.com/theotheradamsmith/verbose-octo-eureka/logic"
+	"github.com/theotheradamsmith/verbose-octo-eureka/logic"
+)
 
 func process_pixel(pixel color.Color) (uint8, error) {
 	r, g, b, a := pixel.RGBA()
-	if r == g && r == b && a == 65535 {
+	if r == g && g == b && a == 65535 {
 		return uint8(r * 255 / 65535), nil
 	}
 	return 0, errors.New("Invalid pixel coloration detected")
@@ -36,11 +36,6 @@ func decode(imagePath string) (string, error) {
 		return "", ok
 	}
 
-	//fmt.Println("Decoded Image: ", decodedImage)
-	//fmt.Println("Bounds: ", decodedImage.Bounds())
-	//fmt.Println("", decodedImage.ColorModel())
-	//fmt.Printf("%T\n", decodedImage.Bounds())
-
 	if decodedImage.Bounds().Dx() != 27 || decodedImage.Bounds().Dy() != 27 {
 		msg := fmt.Sprintf(
 			"Image provided is of invalid size %dx%d",
@@ -49,23 +44,30 @@ func decode(imagePath string) (string, error) {
 		)
 		return "", errors.New(msg)
 	}
-	//fmt.Printf("decodedImage.Bounds().Dx: %d\n", decodedImage.Bounds().Dx())
-	//fmt.Printf("decodedImage.Bounds().Dy: %d\n", decodedImage.Bounds().Dy())
 
 	// upper-left:  (Bounds().Min.X, Bounds().Min.Y)
 	// lower-right: (Bounds().Max.X-1, Bounds().Max.Y-1)
 	for y := decodedImage.Bounds().Min.Y; y < decodedImage.Bounds().Max.Y; y += 3 {
 		for x := decodedImage.Bounds().Min.X; x < decodedImage.Bounds().Max.X; x += 3 {
-			if p, ok := process_pixel(decodedImage.At(x, y)); ok == nil {
-				hexfmtstr := fmt.Sprintf("%x", p)
-				hexfmtint, _ := strconv.Atoi(hexfmtstr)
-				//fmt.Printf("%v", hexfmtint%10)
-				letter := strconv.Itoa(hexfmtint % 10)
-				imageStr.WriteString(letter)
+			pixel1 := decodedImage.At(x, y)
+			pixel2 := decodedImage.At(x+1, y)
+			pixel3 := decodedImage.At(x+2, y)
+			if pixel1 == pixel2 && pixel2 == pixel3 {
+				if p, ok := process_pixel(pixel1); ok == nil {
+					if hexfmtint, ok := strconv.Atoi(fmt.Sprintf("%x", p)); ok != nil {
+						return "", ok
+					} else {
+						letter := strconv.Itoa(hexfmtint % 10)
+						imageStr.WriteString(letter)
+					}
+				} else {
+					return "", ok
+				}
+			} else {
+				msg := fmt.Sprintf("Mismatched pixels: {%v,%v,%v}", pixel1, pixel2, pixel3)
+				return "", errors.New(msg)
 			}
-
 		}
-		//fmt.Println()
 	}
 
 	return imageStr.String(), nil
@@ -75,9 +77,17 @@ func main() {
 	fmt.Println("Hello, CTF!")
 	pFlag := flag.String("path", "", "path of the image to decode")
 	flag.Parse()
-	_, ok := decode(*pFlag)
-	if ok != nil {
-		fmt.Println(ok)
-		return
+	if *pFlag != "" {
+		object, ok := decode(*pFlag)
+		if ok != nil {
+			fmt.Println(ok)
+			return
+		}
+		if _, ok := logic.Check(object); ok != nil {
+			fmt.Println(ok)
+		} else {
+			fmt.Println("Congratulations! You have solved the puzzle!")
+		}
 	}
+	// read from .config file
 }
