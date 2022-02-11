b5a70e7da2cb4d099b50621715f145f1fb7952b0
Adam Smith <adams@cipafilter.com>
2022-02-11 12:16:39 -0600

    Add package main

---
+++ main/go.mod
@@ -0,0 +1,5 @@
+module github.com/theotheradamsmith/verbose-octo-eureka/main
+
+go 1.17
+
+require github.com/theotheradamsmith/verbose-octo-eureka/logic v0.1.1
---
+++ main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/theotheradamsmith/verbose-octo-eureka/logic"
+)
+
+const Name = logic.Name
+
+func main() {
+	fmt.Println(Name)
+}
